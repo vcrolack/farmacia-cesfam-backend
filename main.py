d11f6139f232e390281ae6c72bdcf14c1a53645a
Victor Rolack <victorolack@gmail.com>
2022-05-22 00:40:27 -0400

    entorno creado

---
+++ main.py
@@ -0,0 +1,21 @@
+#python
+from typing import List
+import json
+
+#Pydantic
+
+#FastAPI
+from fastapi import FastAPI
+from fastapi import status
+from fastapi import Body
+
+#Models
+
+app = FastAPI()
+
+
+# PATH OPERATIONS
+
+## USERS
+
+### Create user
---
+++ models.py
@@ -0,0 +1,8 @@
+#Python
+from datetime import date, datetime
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
