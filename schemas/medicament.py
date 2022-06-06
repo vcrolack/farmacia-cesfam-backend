ffb1b353abcdc282b74f39748dcd02194798d24f
Victor Rolack <victorolack@gmail.com>
2022-06-05 23:44:01 -0400

    medicament routes created

--- schemas/medicament.py
+++ schemas/medicament.py
@@ -1,37 +1,43 @@
 # Python
 from datetime import date
 
+# FastAPI
+from fastapi import Form
+
 # Pydantic
 from pydantic import BaseModel
 from pydantic import Field
 
 class Medicament(BaseModel):
-    name: str = Field(
+    name: str = Form(
         ...,
         min_length=1,
         max_length=30
     )
-    grammage: int = Field(...)
-    stock: int = Field(...)
-    format_medicament: int = Field(
-        ...,
-        min_length=1,
-        max_length=25
-    )
-    type_medicament_id: int = Field(...)
-    laboratory_name: str = Field(
+    grammage: int = Form(...)
+    stock: int = Form(...)
+    format_medicament: str = Form(
         ...,
         min_length=1,
         max_length=30
     )
-    principal_agent: str = Field(
+    type_medicament_id: int = Form(...)
+    laboratory_name: str = Form(
         ...,
         min_length=1,
         max_length=30
     )
-    secondary_agent: str = Field(
+    principal_agent: str = Form(
         ...,
         min_length=1,
         max_length=30
     )
-    caducity_date: date = Field(...)
+    secondary_agent: str = Form(
+        ...,
+        min_length=1,
+        max_length=30
+    )
+    caducity_date: date = Form(...)
+
+    class Config:
+        orm_mode = True
