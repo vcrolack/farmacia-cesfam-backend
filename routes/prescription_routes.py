9ed1eac202e9da7ba9f0ebd6b46b5232e639597c
Victor Rolack <victorolack@gmail.com>
2022-06-10 22:05:13 -0400

    added prescription routes

---
+++ routes/prescription_routes.py
@@ -0,0 +1,70 @@
+# Python
+from typing import List
+
+# FastAPI
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import status
+
+# Project imports
+from schemas.prescription import Prescription
+from config.db import get_db
+from services.prescription_service import get_prescriptions, get_prescription, update_prescription, create_prescription, delete_prescription
+
+prescription_routes = APIRouter()
+
+@prescription_routes.get(
+    path="/prescriptions",
+    tags=["prescriptions"],
+    response_model=List[Prescription],
+    status_code=status.HTTP_400_BAD_REQUEST,
+    dependencies=[Depends(get_db)],
+    summary="Get all prescriptions"
+)
+def get_all_prescriptions():
+    prescriptions = get_prescriptions()
+    return prescriptions
+
+@prescription_routes.post(
+    path="/prescriptions",
+    tags=["prescriptions"],
+    response_model=Prescription,
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(get_db)],
+    summary="Create a new prescription"
+)
+def create_a_prescription(prescription: Prescription):
+    return create_prescription(prescription)
+
+@prescription_routes.put(
+    path="/prescriptions/{prescription_id}",
+    tags=["prescriptions"],
+    response_model=Prescription,
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_db)],
+    summary="Update a prescription"
+)
+def update_a_prescription(prescription_id: int, prescription: Prescription):
+    return update_prescription(prescription_id, prescription)
+
+@prescription_routes.get(
+    path="/prescriptions/{prescription_id}",
+    tags=["prescriptions"],
+    response_model=Prescription,
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_db)],
+    summary="Get a prescription"
+)
+def get_a_prescription(prescription_id: int):
+    return get_prescription(prescription_id)
+
+@prescription_routes.delete(
+    path="/prescriptions/{prescription_id}",
+    tags=[Depends(get_db)],
+    response_model=Prescription,
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_db)],
+    summary="Delete a prescription"
+)
+def delete_a_prescription(prescription_id: int):
+    return delete_prescription(prescription_id)
