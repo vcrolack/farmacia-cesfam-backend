22912112cabc683de3a701197d938dffe9485f1c
Victor Rolack <victorolack@gmail.com>
2022-06-21 18:13:06 -0400

    add get a specialty endpoint

--- routes/specialty_routes.py
+++ routes/specialty_routes.py
@@ -9,7 +9,7 @@ from fastapi import status
 # Project imports
 from schemas.specialty import Specialty
 from config.db import get_db
-from services.specialty_service import get_specialties
+from services.specialty_service import get_specialties, get_specialty
 
 specialty_routes = APIRouter()
 
@@ -24,3 +24,14 @@ specialty_routes = APIRouter()
 def get_all_specialties():
   specialties = get_specialties()
   return specialties
+
+@specialty_routes.get(
+  path="/specialties/{specialty_id}",
+  tags=["specialties"],
+  response_model=Specialty,
+  status_code=status.HTTP_200_OK,
+  dependencies=[Depends(get_db)],
+  summary="Get a specialty"
+)
+def get_a_specialty(specialty_id: int):
+  return get_specialty(specialty_id)
